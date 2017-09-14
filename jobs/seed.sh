6bf0a0c065297c3670c2cabe58d6ce4203aa19c8
Steve Neuharth <steve.neuharth@govdelivery.com>
2017-09-13 21:52:20 -0500

    change root repo and build our own spinnaker docker images.

--- jobs/seed.sh
+++ jobs/seed.sh
@@ -2,11 +2,11 @@
 cd /root
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 
-#for job in front50 clouddriver rosco orca gate igor
-#do
-#        echo "building $job"
-#	java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-$job --username jenkins --password jenkins -s
-#done
+# for job in front50 clouddriver rosco orca gate igor deck echo fiat
+# do
+#   echo "building $job"
+# 	java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-$job --username jenkins --password jenkins -s
+# done
 
-echo "building deck"
-java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-deck --username jenkins --password jenkins -s
+# echo "building deck"
+# java -jar jenkins-cli.jar -s http://localhost:8080/ build spinnaker-deck --username jenkins --password jenkins -s
